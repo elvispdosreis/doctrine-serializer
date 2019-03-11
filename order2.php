61ec0068ce3582adc3cea5e83cd71890fe2925d4
elvis <elvisdosreis@gmail.com>
2019-03-11 12:46:31 -0300

    test

--- order2.php
+++ order2.php
@@ -10,22 +10,18 @@ $em = getEntityManager();
 $connection = \Doctrine\DBAL\DriverManager::getConnection($dbParams);
 
 
-
-
-
-
 $group = \JMS\Serializer\SerializationContext::create()->setGroups(['details']);
 $group->setSerializeNull(true);
 
 
 
 
-$json = '{"id":3, "series":4,"key":"854478557872588552699","number":1,"date":"2019-03-08","xml":"<?xml version=\"1.0\" encoding=\"UTF-8\"?>"}';
+$json = '{"id":1,"series":4,"key":"854478557872588552699","number":1,"date":"2019-03-08","xml":"<?xml version=\"1.0\" encoding=\"UTF-8\"?>"}';
 $em = $registry->getManager();
 
 $invoice = $serializer->deserialize($json, \SON\Entity\Invoice::class, 'json');
 if($entityManager->getUnitOfWork()->getEntityState($invoice) === \Doctrine\ORM\UnitOfWork::STATE_NEW){
-    $order = $entityManager->getRepository(\SON\Entity\Order::class)->find(3);
+    $order = $entityManager->getRepository(\SON\Entity\Order::class)->find(1);
     $invoice->setOrder($order);
     $em->persist($invoice);
 }
