fcea365eb629a09775a74cb3f5fa010524e54cf8
elvis <elvisdosreis@gmail.com>
2019-03-13 08:58:39 -0300

    test

--- category.php
+++ category.php
@@ -7,21 +7,14 @@ require_once __DIR__ . '/src/doctrine.php';
 
 $entityManager = getEntityManager();
 
+$arr = explode('>', 'Informática > Componentes para PC > Fontes de Alimentação > ATX > 500W a 590W');
+$arr = array_map('trim', $arr);
+$arr = new \Doctrine\Common\Collections\ArrayCollection($arr);
 
 $entity = $entityManager->getReference(\SON\Entity\Category::class, 1);
-//https://wiki.php.net/rfc/iterable
 $category = new \SON\Entity\Category();
-$children->setName('Informática');
+$category->setName('Informática');
 $category->setParent($entity);
 
-$arr = explode('>', 'Informática > Componentes para PC > Fontes de Alimentação > ATX > 500W a 590W');
-foreach ($arr as $key => $value) {
-     $parent = $entityManager->getRepository(\SON\Entity\Category::class)->findOneBy(['name' => trim($value)]);
-     $children = new \SON\Entity\Category();
-     $children->setName(trim($value));
-     $entity->setChildren($children);
-}
-
-$entityManager->persist($children);
+$entityManager->persist($category);
 $entityManager->flush();
-
